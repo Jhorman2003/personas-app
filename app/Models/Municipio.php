15338cea70a19a10efc8325203c3febd5107bbe1
Jhorman Montoya <jhormaparaelcreador@gmail.com>
2024-04-02 21:26:25 -0500

    Initial project M

--- app/Models/Municipio.php
+++ app/Models/Municipio.php
@@ -5,10 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Municipio extends Model
+class municipio extends Model
 {
     use HasFactory;
-    protected $table = "tb_municipio";
-    protected $primaryKey = 'muni_codi';
-    public $timestamps = false;
+    protected $table = 'tb_municipio';
+    protected $primaryKey= 'muni_codi';
+    public $timestamps=false;
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
-
-use App\Http\Controllers\ComunaController;
+use App\Http\Controllers\MunicipioController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,14 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/comunas',[ComunaController::class, 'index'])->name('comunas.index');
-Route::post('/comunas',[ComunaController::class, 'store'])->name('comunas.store');
-Route::get('/comunas/create',[ComunaController::class, 'create'])->name('comunas.create');
-Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])->name('comunas.destroy');
-Route::put('/comunas/{comuna}',[ComunaController::class, 'update'])->name('comunas.update');
-Route::get('/comunas/{comuna}/edit',[ComunaController::class, 'edit'])->name('comunas.edit');
-
-
-
-
+Route::get('/municipios',[MunicipioController::class, 'index'])->name('municipios.index');
